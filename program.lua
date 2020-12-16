506949a4200234916de321b719a0573bd6dbad58
tristan-tr <60301459+tristan-tr@users.noreply.github.com>
2020-12-16 19:20:56 +0100

    Update program.lua

--- program.lua
+++ program.lua
@@ -120,25 +120,6 @@ function updateTerm(reactorInfo)
 	GraphicsAPI.writeText("Last Emergency Action: "..lastEmergencyAction, {1,19}, colors.gray)
 end
 
--- https://pastebin.com/t9ETGyZk
--- Tweakable PID gains
- 
-local inflowPGain = 1
-local inflowIGain = 0.04
-local inflowDGain = 0.1
- 
-local outflowPGain = 500
-local outflowIGain = 0.5
-local outflowIIGain = 0.0000003
-local outflowDGain = 60000
-
-local inflowISum = 0
-local inflowDLast = 0
- 
-local outflowISum = 0
-local outflowIISum = 0
-local outflowDLast = 0
-
 -- Main loop
 while true do
 	for i=1,monitorUpdateTime/sleepTime do
@@ -146,45 +127,30 @@ while true do
 
 		saturationPercentage = (reactorInfo.energySaturation / reactorInfo.maxEnergySaturation) * 100
 		fieldPercentage = reactorInfo.fieldStrength / 1000000
-		fuelPercentage = 100 - ((reactorInfo.fuelConversion / reactorInfo.maxFuelConversion) * 100)
+		fuelConversion = reactorInfo.fuelConversion / reactorInfo.maxFuelConversion
+		fuelPercentage = 100 - (fuelConversion * 100)
 
 		updateTerm(reactorInfo)
 
 		if reactorInfo.status == "running" then
-			-- https://pastebin.com/t9ETGyZk
-			-- Change our input gate to keep field strength at our target
-		    local fieldError = (reactorInfo.maxFieldStrength * (targetFieldPercentage / 100)) - reactorInfo.fieldStrength
-		    local proportionalFieldError = fieldError * inflowPGain
-		    inflowISum = inflowISum + fieldError
-		    local integralFieldError = inflowISum * inflowIGain
-		    local derivativeFieldError = (fieldError - inflowDLast) * inflowDGain
-		    inflowDLast = fieldError
-		    local inflow = proportionalFieldError + integralFieldError + derivativeFieldError
-		    if inflow < 0 then
-		      inflowISum = inflowISum - fieldError
-		    end
+			-- Formulas are derived from the draconic evolution source
+			-- https://github.com/brandon3055/Draconic-Evolution/blob/866f17aa9bae1266455d8d86d3417331396c252f/src/main/java/com/brandon3055/draconicevolution/blocks/reactor/tileentity/TileReactorCore.java#L270
 
-		    inputFluxGate.setSignalLowFlow(inflow);
+		    -- Change our input gate to keep the field percentage at our target
+		    inputFluxGate.setSignalLowFlow(reactorInfo.fieldDrainRate / (1 - (targetFieldPercentage/100)));
 
-			-- Change our output gate to keep temperature at our target
-		    local tempError = targetTemperature - reactorInfo.temperature
-		    local proportionalTempError = tempError * outflowPGain
-		    outflowISum = outflowISum + tempError
-		    local integralTempError = outflowISum * outflowIGain
-		    if math.abs(tempError) < 100 then
-		    	outflowIISum = outflowIISum + integralTempError
-		    else
-		        outflowIISum = 0
-		    end
-		    local secondIntegralTempError = outflowIISum * outflowIIGain
-		    local derivativeTempError = (tempError - outflowDLast) * outflowDGain
-		    outflowDLast = tempError
-		    local outflow = proportionalTempError + integralTempError + secondIntegralTempError + derivativeTempError
-		    if outflow < 0 then
-		        outflowISum = outflowISum - tempError
-		    end
+		    -- Change our output gate to keep temperature at our target
+		    newSat = reactorInfo.maxEnergySaturation * 0.80
 
-		    outputFluxGate.setSignalLowFlow(outflow)
+		    flux = reactorInfo.energySaturation + reactorInfo.generationRate - newSat
+
+		    coreSat = newSat / reactorInfo.maxEnergySaturation
+		    negCSat = (1 - coreSat) * 99
+		    temp50 = math.min(reactorInfo.temperature / 200, 99)
+		    convLVL = fuelConversion * 1.3 - 0.3
+
+
+		    outputFluxGate.setSignalLowFlow(flux)
 		else
 			if reactorInfo.status == "warming_up" then
 				-- We are charging so we need to set our input gate to allow RF
@@ -210,7 +176,7 @@ while true do
 			reactor.stopReactor()
 			if emergencyChargeIsSetup then
 				reactor.chargeReactor()
-				emergencyChargeGate.setSignalLowFlow(200000)
+				emergencyChargeGate.setSignalLowFlow(10000000)
 			end
 			lastEmergencyAction = "Field Percentage < "..lowestFieldPercentage
 		else
